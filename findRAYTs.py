c20db3394e20b979ed68952265d9fa2afd11220a
Eric Hugoson <eric@hugoson.org>
2017-09-21 17:19:57 +0200

    added dynamic output function

--- findRAYTs.py
+++ findRAYTs.py
@@ -4,26 +4,60 @@ from __future__ import print_function
 from Bio import SeqIO
 from micomplete import calcCompleteness
 from micomplete import micomplete
+from contextlib import contextmanager
+from distutils import spawn
+from collections import defaultdict
 import sys
 import re
 import argparse
 import shutil
-import spawn
 import multiprocessing as mp
 import subprocess
 
-def worker(fasta,  name, hmm, evalue=1e-20):
+def worker(fasta, seqType, name, hmm, evalue=1e-20, outfile=sys.stdout):
+    if seqType == "faa":
+        faa = fasta
+    elif seqType == "fna":
+        faa = micomplete.create_proteome(fasta, name)
+    elif re.match("(gb.?.?)|genbank", seqType):
+        faa = micomplete.cextract_gbk_trans(fasta, name)
+    else:
+        raise TypeError('Sequence type needs to be one of faa/fna/gbk')
+    get_RAYTs(faa, name, hmm, evalue)
+
+def listener(q):
+    """Process to write results in a thread safe manner"""
     pass
 
-def get_RAYTs(fasta, name, hmm, evalue=1e-20):
-    if not fasta == "faa":
-        faa = micomplete.extract_gbk_trans(fasta, name)
-        faa = micomplete.create_proteome(fasta, name)
+@contextmanager
+def open_stdout(outfile):
+    """Function dynamically allows printing to file or stdout when used as open 
+    with 'with'. No filename or '-' results in stdout, any other name will be
+    used as filename to be written"""
+    if outfile and outfile != '-':
+        handle = open(outfile, 'w')
     else:
-        faa = fasta
-    comp = calcCompleteness(fasta, name, hmm, evalue)
-    
-    pass
+        handle = sys.stdout
+    # close open file if not stdout
+    try:
+        yield handle
+    finally:
+        if handle is not sys.stdout:
+            handle.close()
+
+def get_RAYTs(faa, name, hmm, evalue=1e-20):
+    comp = calcCompleteness(faa, name, hmm, evalue)
+    foundRAYTs, dupRAYTs, totl = comp.get_completeness()
+
+    # ensure unique, best RAYT match for each gene
+    # but allow infinite gene matches for each RAYT
+    gene_matches = defaultdict(list)
+    for RAYT, match in foundRAYTs.items():
+        for gene, evalue in match:
+            if gene not in gene_matches:
+                gene_matches[gene].append([RAYT, evalue])
+        print(gene_matches[gene])
+            
 
 
 def main():
@@ -53,19 +87,19 @@ def main():
         except AssertionError:
             raise RuntimeError('Unable to find hmmsearch in path')
 
-    with open(args.sequence) as seq_file:
+    with open(args.fastaList) as seq_file:
         inputSeqs = [ seq.strip().split('\t') for seq in seq_file ]
-
-    manager = mp.Manger()
+    print(inputSeqs)
+    manager = mp.Manager()
     q = manager.Queue()
-    pool = mp.Pool(process=args.threads + 1)
+    pool = mp.Pool(processes=args.threads + 1)
     # init listener here
 
     jobs = []
     for i in inputSeqs: 
         if len(i) == 2:
             i.append(None)
-        job = pool.apply_async(worker(i[0], i[1], i[2]))
+        job = pool.apply_async(worker(i[0], i[1], i[2], args.hmms))
         jobs.append(job)
     # get() all processes to catch errors
     for job in jobs:
