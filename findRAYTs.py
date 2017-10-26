3fe2e54748e4c857666c6c2bfa146cc307e10a04
Eric Hugoson <eric@hugoson.org>
2017-10-26 17:25:58 +0200

    early parsing of map file

--- findRAYTs.py
+++ findRAYTs.py
@@ -111,6 +111,37 @@ class parse_taxonomy():
                     break
         return list(zip(ranks, taxids))
 
+class parseMapFile():
+    def __init__(self, map_file, query_column, query_selection):
+        self.map_file = map_file
+        self.query_column = query_column
+        self.query_selection = query_selection
+        gene_files = open(self.map_file, 'r+b')
+        self.gene_mem = mmap.mmap(gene_files(), 0, prot=mmap.PROT_READ)
+        headers = self.gene_mem.readline().split()
+        self.col = 0
+        for header in headers:
+            if header == self.query_column:
+                break
+            self.col += 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # here sort of what column to return
+        return gene_paths()
+
+    def gene_paths(self):
+        """Extracts and outputs iterable of all gene pathes matching query in 
+        column"""
+        for mapping in self.gene_mem.split():
+            if self.query_selection in mapping[self.col]:
+                # column 11 corresponds to gene_path in mapping file
+                return mapping[11]
+        self.gene_mem.seek(0)
+        raise StopIteration
+
 def _worker(fasta, seqType, name, hmm, q, gen_directory, evalue=1e-20, outfile=None):
     tax = parse_taxonomy()
     if seqType == "faa":
@@ -331,6 +362,9 @@ def main():
         except AssertionError:
             raise RuntimeError('Unable to find hmmsearch in path')
 
+    for i in range(5):
+        parseMapFile("reference_results.tsv", "Genus", "Shewanella")
+    sys.exit()
     # Initialise taxdump, threadsafety
     parse_taxonomy()
 
